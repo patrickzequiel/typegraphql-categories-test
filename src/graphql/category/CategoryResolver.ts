18de08c9b8f571fd3ce75980aa0f2ae119168016
Patrick Cruz <patrickezequielscruz@gmail.com>
2022-02-08 22:41:39 +0100

    finished but needs correction on description

--- src/graphql/category/CategoryResolver.ts
+++ src/graphql/category/CategoryResolver.ts
@@ -1,10 +1,17 @@
-import { Query, Resolver } from 'type-graphql';
+import { Arg, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
 import Category from './Category';
 import CategorySchema from '../../model/CategorySchema';
 //first, create a search inside the database
 //when we want to search, use a query
 //input, we mutate
 
+@InputType() //this defines that our class will be an input class
+class CategoryInput { 
+    @Field()
+    description: string;
+    @Field()
+    name: string;
+}
 
 //to define that this class became a resolver, we need to import the resolver class
 @Resolver(Category) //this class will be a resolver for the Category class
@@ -15,6 +22,14 @@ class CategoryResolver {
         const categories = await CategorySchema.find(); //this will search all categories
         return categories; //this will return all categories
     }
+
+    @Mutation(() => Category) 
+    async createCategory(@Arg("categoryInput") categoryInput:  CategoryInput) {
+        const category = await CategorySchema.create(categoryInput);
+        return category;
+    }
+
 }
+
 
 export default CategoryResolver;
